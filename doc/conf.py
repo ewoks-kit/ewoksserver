5ce91f442a5f5a9147aa5b59174fcc0b4a98d0ac
woutdenolf <woutdenolf@users.sf.net>
2022-09-28 10:49:25 +0200

    improve docs

--- doc/conf.py
+++ doc/conf.py
@@ -6,14 +6,19 @@ release = "0.1"
 copyright = "2021, ESRF"
 author = "ESRF"
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinxcontrib.redoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.redoc",
+]
 templates_path = ["_templates"]
 exclude_patterns = []
 
 autodoc_typehints = "description"
 autodoc_typehints_description_target = "all"
 
-html_theme = "alabaster"
+html_theme = "classic"
 html_static_path = []
 
 autosummary_generate = True
