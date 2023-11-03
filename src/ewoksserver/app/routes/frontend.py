1b3e5f631ceb2a6f964c21d0e8cb125319c1fee8
woutdenolf <woutdenolf@users.sf.net>
2023-11-03 11:04:03 +0100

    fix /edit and /monitor paths

--- src/ewoksserver/app/routes/frontend.py
+++ src/ewoksserver/app/routes/frontend.py
@@ -1,6 +1,8 @@
 import logging
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import Response
+from starlette.types import Scope
 
 try:
     from ewoksweb.serverutils import get_static_root
@@ -11,11 +13,17 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
+class _StaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope: Scope) -> Response:
+        """This gets called for every path that is not mounted or route that is not included."""
+        if path in ("edit", "monitor"):
+            path = ""
+        return await super().get_response(path, scope)
+
+
 def add_frontend(app: FastAPI) -> None:
     if get_static_root is None:
         logger.info("No frontend available to serve (pip install ewoksweb)")
     else:
-        files = StaticFiles(directory=get_static_root(), html=True)
+        files = _StaticFiles(directory=get_static_root(), html=True)
         app.mount("/", app=files, name="ewoksweb")
-        app.mount("/edit", app=files, name="ewoksweb_edit")
-        app.mount("/monitor", app=files, name="ewoksweb_monitor")
