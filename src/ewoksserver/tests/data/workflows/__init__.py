7347a235d3f138c3dfb759900fd6c26e79537238
Giannis Koumoutsos <koumoutsosgian@yahoo.gr>
2024-03-07 17:50:29 +0100

    create a workflow to run the tests

---
+++ src/ewoksserver/tests/data/workflows/__init__.py
@@ -0,0 +1,11 @@
+import os
+import json
+from typing import List
+
+_SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_workflow(name:str) -> List[str]:
+    filename = os.path.join(_SCRIPT_DIR, name)
+    with open(filename, "rb") as f:
+        return json.load(f)
